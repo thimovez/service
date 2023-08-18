8882f9c56630f65070c0a752a6e5da48184901ac
Vasyl Kovalenko <thimovez@gmail.com>
2023-08-18 16:29:37 +0300

    add config

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,47 +1,17 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
-	"github.com/thimovez/service/pkg/api/user"
-	"github.com/thimovez/service/pkg/repository"
-	"net/http"
-	"os"
+	"github.com/thimovez/service/config"
+	"github.com/thimovez/service/internal/app"
+	"log"
 )
 
 func main() {
-	err := run()
+	// Configuration
+	cfg, err := config.NewConfig()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "this is the startup error: %s\n", err)
-		os.Exit(1)
-	}
-}
-
-func run() error {
-	connectionString := "postgres://postgres:postgres@localhost/**NAME-OF-YOUR-DATABASE-HERE**?sslmode=disable"
-	db, err := setupDatabase(connectionString)
-	if err != nil {
-		return err
-	}
-	storage := repository.NewStorage(db)
-	http.HandleFunc("/login", user.Login)
-
-	http.ListenAndServe(":8080", nil)
-}
-
-func setupDatabase(connString string) (*sql.DB, error) {
-	db, err := sql.Open("postgres", connString)
-
-	if err != nil {
-		return nil, err
+		log.Fatalf("Config error: %s", err)
 	}
 
-	// ping the DB to ensure that it is connected
-	err = db.Ping()
-
-	if err != nil {
-		return nil, err
-	}
-
-	return db, nil
+	app.Run(cfg)
 }
---
+++ config/config.go
@@ -0,0 +1,43 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type (
+	// Config -.
+	Config struct {
+		HTTP `yaml:"http"`
+		PG   `yaml:"postgres"`
+	}
+
+	// HTTP -.
+	HTTP struct {
+		Port string `env-required:"true" yaml:"port" env:"HTTP_PORT"`
+	}
+
+	// PG -.
+	PG struct {
+		PoolMax int    `env-required:"true" yaml:"pool_max" env:"PG_POOL_MAX"`
+		URL     string `env-required:"true" yaml:"pg_url"  env:"PG_URL"`
+	}
+)
+
+// NewConfig returns app config.
+func NewConfig() (*Config, error) {
+	cfg := &Config{}
+
+	err := cleanenv.ReadConfig("./config/config.yml", cfg)
+	if err != nil {
+		return nil, fmt.Errorf("config error: %w", err)
+	}
+
+	err = cleanenv.ReadEnv(cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	return cfg, nil
+}
