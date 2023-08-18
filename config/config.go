33ae58046ac6a27147361e01a7f8bfa8c7176975
Vasyl Kovalenko <thimovez@gmail.com>
2023-08-18 19:53:48 +0300

    add config files

--- config/config.go
+++ config/config.go
@@ -20,8 +20,7 @@ type (
 
 	// PG -.
 	PG struct {
-		PoolMax int    `env-required:"true" yaml:"pool_max" env:"PG_POOL_MAX"`
-		URL     string `env-required:"true" yaml:"pg_url"  env:"PG_URL"`
+		URL string `env-required:"true" yaml:"pg_url"  env:"PG_URL"`
 	}
 )
 
@@ -29,7 +28,7 @@ type (
 func NewConfig() (*Config, error) {
 	cfg := &Config{}
 
-	err := cleanenv.ReadConfig("./config/config.yml", cfg)
+	err := cleanenv.ReadConfig("../../config/config.yml", cfg)
 	if err != nil {
 		return nil, fmt.Errorf("config error: %w", err)
 	}
