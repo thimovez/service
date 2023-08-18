58deadd7da6c655fe3fa84e7c64c4ba6b3aee8d6
Vasyl Kovalenko <thimovez@gmail.com>
2023-08-18 19:52:48 +0300

    refactor migration

--- migrations/20230818095833_init.sql
+++ migrations/20230818095833_init.sql
@@ -2,8 +2,8 @@
 -- +goose StatementBegin
 CREATE TABLE IF NOT EXISTS users (
     id bigserial PRIMARY KEY NOT NULL,
-    username varchar(20) UNIQUE NOT NULL,
-    password_hash varchar(30) NOT NULL
+    username varchar(255) UNIQUE NOT NULL,
+    password_hash varchar(255) NOT NULL
     );
 -- +goose StatementEnd
 
