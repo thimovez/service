786f8205b5669aba4acb60e134110a5c8792813c
Vasyl Kovalenko <thimovez@gmail.com>
2023-08-19 10:07:30 +0300

    add new injection for user

--- internal/app/app.go
+++ internal/app/app.go
@@ -3,10 +3,12 @@ package app
 import (
 	"fmt"
 	"github.com/thimovez/service/config"
-	"github.com/thimovez/service/internal/controller/http1"
+	imageAPI "github.com/thimovez/service/internal/api/image"
+	userAPI "github.com/thimovez/service/internal/api/user"
 	"github.com/thimovez/service/internal/usecase/image"
 	"github.com/thimovez/service/internal/usecase/repo/image-repo"
 	"github.com/thimovez/service/internal/usecase/repo/user-repo"
+	"github.com/thimovez/service/internal/usecase/token"
 	"github.com/thimovez/service/internal/usecase/user"
 	"github.com/thimovez/service/pkg/postgres"
 	"log"
@@ -23,13 +25,14 @@ func Run(cfg *config.Config) {
 	userRepo := user_repo.New(db)
 	imageRepo := image_repo.New(db)
 
-	userCase := user.New(userRepo)
+	tokenCase := token.New()
+	userCase := user.New(userRepo, tokenCase)
 	imageCase := image.New(imageRepo)
 
 	mux := http.NewServeMux()
 
-	http1.NewUserRoutes(mux, userCase)
-	http1.NewImageRoutes(mux, imageCase)
+	userAPI.NewUserRoutes(mux, userCase)
+	imageAPI.NewImageRoutes(mux, imageCase)
 
 	http.ListenAndServe(cfg.HTTP.Port, mux)
 }
