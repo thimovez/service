4f02431fa50d6093d5dd3c5442c1325a779f6df6
Vasyl Kovalenko <thimovez@gmail.com>
2023-08-19 10:07:55 +0300

    add loging response entity

--- internal/entity/entity.go
+++ internal/entity/entity.go
@@ -1,14 +1,18 @@
 package entity
 
 type UserRequest struct {
-	ID       int64  `json:"id"`
+	ID       string `json:"id"`
 	Username string `json:"username"`
 	Password string `json:"password"`
 }
 
 type Image struct {
-	ID        int64  `json:"id"`
-	UserID    int64  `json:"user_id"`
+	ID        string `json:"id"`
+	UserID    string `json:"user_id"`
 	ImagePath string `json:"image_path"`
 	ImageURL  string `json:"image_url"`
+}
+
+type LoginResponse struct {
+	AccessToken string
 }
