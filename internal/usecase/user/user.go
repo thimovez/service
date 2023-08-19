9f758a6d472b8333dac3043837b20deb6a311dcf
Vasyl Kovalenko <thimovez@gmail.com>
2023-08-19 10:09:01 +0300

    add some validation for user

--- internal/usecase/user/user.go
+++ internal/usecase/user/user.go
@@ -1,9 +1,11 @@
 package user
 
 import (
+	"github.com/google/uuid"
 	"github.com/thimovez/service/internal/entity"
 	"github.com/thimovez/service/internal/usecase"
 	"golang.org/x/crypto/bcrypt"
+	"time"
 )
 
 type UserUseCase struct {
@@ -18,17 +20,31 @@ func New(r usecase.UserRepo, t usecase.TokenService) *UserUseCase {
 	}
 }
 
-func (u *UserUseCase) Login(user entity.UserRequest) error {
+func (u *UserUseCase) Login(user entity.UserRequest) (accessToken string, err error) {
+	err = u.repo.CheckUsername(user.Username)
+	if err != nil {
+		return "", err
+	}
+
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
 	if err != nil {
-		return err
+		return "", nil
 	}
 	user.Password = string(hashedPassword)
 
+	id := uuid.New()
+	user.ID = id.String()
+
 	err = u.repo.SaveUser(user)
 	if err != nil {
-		return err
+		return "", nil
 	}
 
-	return nil
+	expiration := time.Now().Add(time.Hour * 12)
+	accessToken, err = u.token.GenerateAccessToken(user.ID, expiration)
+	if err != nil {
+		return "", nil
+	}
+
+	return accessToken, nil
 }
