cff5495164c2d136c979a1d992486a5a0bf1fb2a
Vasyl Kovalenko <thimovez@gmail.com>
2023-08-19 15:04:29 +0300

    add image repo functions

--- internal/usecase/repo/image-repo/image-postgres.go
+++ internal/usecase/repo/image-repo/image-postgres.go
@@ -13,13 +13,29 @@ func New(db *sql.DB) *ImageRepo {
 	return &ImageRepo{db}
 }
 
-//TODO repo
-func (u *ImageRepo) SaveImage(image entity.Image) error {
+func (i *ImageRepo) SaveImage(image entity.Image) error {
+	q := `INSERT INTO images (id, user_id, image_path, image_url) VALUES ($1, $2, $3, $4)`
+
+	_, err := i.db.Exec(q, image.ID, image.UserID, image.ImagePath, image.ImageURL)
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
 
-// TODO
-func (u *ImageRepo) GetImages() error {
-	return nil
+func (i *ImageRepo) GetImages() (images []string, err error) {
+	q := `SELECT * FROM images`
+
+	row, err := i.db.Query(q)
+	if err != nil {
+		return
+	}
+
+	images, err = row.Columns()
+	if err != nil {
+		return
+	}
+
+	return images, nil
 }
