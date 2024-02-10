92792612728d2af1be087cd33ba462621147909d
Vasyl Kovalenko <kovalenkova26@gmail.com>
2024-02-10 13:10:36 +0200

    add comments

--- client/src/http/index.ts
+++ client/src/http/index.ts
@@ -13,21 +13,26 @@ const $authHost = axios.create({
 
 // Interceptor receives the Bearer header and assigns 
 // token received from local storage for every request 
-// made from this instace
+// maden from this instance
 $authHost.interceptors.request.use((config) => {
     config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`
     return config;
 })
 
+// Interceptor refresh token if access token token has expired.
+// For every response maden from this instance
 $authHost.interceptors.response.use((config) => {
     return config;
 },async (error) => {
     const originalRequest = error.config;
     if (error.response.status == process.env.StatusUnauthorized && error.config && !error.config._isRetry) {
+        // Set field isRetry to true to avoid request looping
         originalRequest._isRetry = true;
         try {
+            // Make refresh request to update our tokens
             const response = await axios.get<AuthResponse>(`${API_URL}/refresh`, {withCredentials: true})
             localStorage.setItem('token', response.data.accessToken);
+
             return $authHost.request(originalRequest);
         } catch (e) {
             console.log('Not Authorized')
