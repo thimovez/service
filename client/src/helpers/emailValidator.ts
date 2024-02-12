b53b95b8c0075c8935f76ab18656ae52107c2aeb
Vasyl Kovalenko <kovalenkova26@gmail.com>
2024-02-12 22:11:30 +0200

    move to helper

---
+++ client/src/helpers/emailValidator.ts
@@ -0,0 +1,5 @@
+// Basic email validation using a regular expression
+export const isEmailValid = (email: string) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+};
