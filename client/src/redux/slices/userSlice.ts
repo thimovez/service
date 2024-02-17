d8a7bf94771aa1d9ee0193a1397088b32f59356e
Vasyl Kovalenko <kovalenkova26@gmail.com>
2024-02-17 16:28:25 +0200

    change initial state in user slice

--- client/src/redux/slices/userSlice.ts
+++ client/src/redux/slices/userSlice.ts
@@ -1,18 +1,20 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { IUser } from '../../models/IUser';
+
+const user = {} as IUser
 
 export const counterSlice = createSlice({
     name: 'user',
-    initialState: {
-        loading: false,
-        user: null,
-        error: null
-    },
+    initialState: user,
     reducers: {
-        setUser: (state, action) => {
-            const { user } = action.payload;
-            state.user = user;
+        setUser: (state, action: PayloadAction<IUser>) => {
+            const user  = action.payload;
+            state.email = user.email;
+            state.id = user.id;
         },
     }
 })
+
+export const { setUser } = counterSlice.actions
 
 export default counterSlice.reducer;
