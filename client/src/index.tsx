6e02e5ff6c31adec3cd30158202a333054732902
Vasyl Kovalenko <kovalenkova26@gmail.com>
2024-02-20 19:21:50 -1000

    update root rendering file

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,12 +1,13 @@
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import { Provider } from 'react-redux';
 import {store} from './redux/store';
 
-ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-  document.getElementById('root')
-);
+const container = document.getElementById('root');
+const root = createRoot(container!); 
 
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+);
