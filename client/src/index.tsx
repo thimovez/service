64fb4056fb106ca16928764eb52de50424799706
Vasyl Kovalenko <kovalenkova26@gmail.com>
2024-02-20 19:23:19 -1000

    refactor

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,7 +1,8 @@
 import { createRoot } from 'react-dom/client';
-import App from './App';
 import { Provider } from 'react-redux';
-import {store} from './redux/store';
+
+import { store } from './redux/store';
+import App from './App';
 
 const container = document.getElementById('root');
 const root = createRoot(container!); 
