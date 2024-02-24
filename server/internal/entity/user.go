099b0bd298da773996df54989eb0f76685d6a63a
Vasyl Kovalenko <kovalenkova26@gmail.com>
2024-02-24 17:21:21 +0200

    add extra entity for user

--- server/internal/entity/user.go
+++ server/internal/entity/user.go
@@ -5,3 +5,14 @@ type UserRequest struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
 }
+
+type User struct {
+	ID       string `json:"id"`
+	Username string `json:"username"`
+}
+
+type UserResponse struct {
+	Success bool   `json:"success"`
+	Message string `json:"message"`
+	Data    User   `json:"data"`
+}
