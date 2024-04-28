936518a84b5b76b71536f47d0d982a568c09ec45
Vasyl Kovalenko <kovalenkova26@gmail.com>
2024-04-28 23:35:04 +0300

    refactor

--- server/migrations/20230818095833_init.sql
+++ server/migrations/20230818095833_init.sql
@@ -1,10 +1,10 @@
 -- +goose Up
 -- +goose StatementBegin
 CREATE TABLE IF NOT EXISTS users (
-    id text PRIMARY KEY NOT NULL,
-    username varchar(50) UNIQUE NOT NULL,
+    id TEXT PRIMARY KEY NOT NULL,
+    username VARCHAR(50) UNIQUE NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
-    password_hash varchar(255) NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     role VARCHAR(20) NOT NULL DEFAULT 'user'
